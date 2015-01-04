d1e5232ea4380ae97de75dcffcc7cdf8700ba285
MikeCAT <mikecat@users.noreply.github.com>
2015-01-04 22:16:47 +0900

    added comments

--- usbio_windows.h
+++ usbio_windows.h
@@ -3,11 +3,17 @@
 
 struct avrio_t;
 
-/* USB-IO2.0を用いた通信を初期化する */
+/* USB-IO2.0を用いた通信を初期化する。
+ * 成功と判定したら真、失敗を検出したら偽を返す。
+ */
 int usbio_init(struct avrio_t *avrio, int sin_port, int sout_port, int reset_port);
-/* USB-IO2.0を用いた通信を終了する */
+/* USB-IO2.0を用いた通信を終了する。
+ * 成功と判定したら真、失敗を検出したら偽を返す。
+ */
 int usbio_stop(struct avrio_t *avrio);
-/* USB-IO2.0を用いて8ビット送受信する */
+/* USB-IO2.0を用いて8ビット送受信する。
+ * 成功したら受信したデータを、失敗したら-1を返す。
+ */
 int usbio_io_8bits(void *hardware_data, int out);
 
 #endif
