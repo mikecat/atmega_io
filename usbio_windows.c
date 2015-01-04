1ae92720d8eb0e133b85c3714ba4feada37a2634
MikeCAT <mikecat@users.noreply.github.com>
2015-01-04 22:25:27 +0900

    implement usbio_init

--- usbio_windows.c
+++ usbio_windows.c
@@ -7,18 +7,19 @@
 #include "avr_io.h"
 
 #define IO_SIZE 65
+#define PORT_NUM 12
 
 typedef struct {
 	HANDLE hDevice;
 	int sin_port, sout_port, reset_port;
 } hid_t;
 
-static int openHID(hid_t *hid, int vendor_id, const int product_ids[], int product_id_num) {
+static int openHID(HANDLE *hHid, int vendor_id, const int product_ids[], int product_id_num) {
 	GUID hid_guid;
 	HDEVINFO hDeviceInfo;
 	int index;
 	HidD_GetHidGuid(&hid_guid);
-	if (hid == NULL || product_ids == NULL || product_id_num <= 0) return 0;
+	if (product_ids == NULL || product_id_num <= 0) return 0;
 
 	/* デバイスを列挙して探す */
 	hDeviceInfo = SetupDiGetClassDevs(
@@ -67,7 +68,7 @@ static int openHID(hid_t *hid, int vendor_id, const int product_ids[], int produ
 						caps.OutputReportByteLength == IO_SIZE &&
 						caps.InputReportByteLength == IO_SIZE) {
 							/* 結果を返す */
-							hid->hDevice = hDevice;
+							*hHid = hDevice;
 							SetupDiDestroyDeviceInfoList(hDeviceInfo);
 							return 1;
 						}
@@ -103,11 +104,36 @@ static int inputAndOutput(HANDLE hUsbIO,int writeData,int *readData) {
 	return 1;
 }
 
-int usbio_init(struct avrio_t *avrio, int sin_port, int sout_port, int reset_port) {
-	return 0;
+int usbio_init(avrio_t *avrio, int sin_port, int sout_port, int reset_port) {
+	static const int vendor_id = 0x1352;
+	static const int product_id[2] = {0x120, 0x121};
+	static const int product_id_num = 2;
+	HANDLE hUsbIO;
+	hid_t *hid;
+	if (avrio == NULL) return 0;
+	if (sin_port < 0 || PORT_NUM <= sin_port || sout_port < 0 || PORT_NUM <= sout_port ||
+	reset_port < 0 || PORT_NUM <= reset_port || sin_port == sout_port ||
+	sout_port == reset_port || reset_port == sin_port) {
+		/* 無効なポートまたはポートが被っている */
+		return 0;
+	}
+	/* USB-IO2.0を開く */
+	if(!openHID(&hUsbIO, vendor_id, product_id, product_id_num)) {
+		return 0;
+	}
+	/* 情報を格納する */
+	hid = malloc(sizeof(hid_t));
+	if (hid == NULL) return 0;
+	hid->hDevice = hUsbIO;
+	hid->sin_port = sin_port;
+	hid->sout_port = sout_port;
+	hid->reset_port = reset_port;
+	avrio->hardware_data = (void*)hid;
+	avrio->io_8bits = usbio_io_8bits;
+	return 1;
 }
 
-int usbio_stop(struct avrio_t *avrio) {
+int usbio_stop(avrio_t *avrio) {
 	return 0;
 }
 
--- usbio_windows.h
+++ usbio_windows.h
@@ -1,16 +1,16 @@
 #ifndef USBIO_WINDOWS_H_GUARD_618B5E6E_9646_432F_97B9_A7EAD7641536
 #define USBIO_WINDOWS_H_GUARD_618B5E6E_9646_432F_97B9_A7EAD7641536
 
-struct avrio_t;
+#include "avr_io.h"
 
 /* USB-IO2.0を用いた通信を初期化する。
  * 成功と判定したら真、失敗を検出したら偽を返す。
  */
-int usbio_init(struct avrio_t *avrio, int sin_port, int sout_port, int reset_port);
+int usbio_init(avrio_t *avrio, int sin_port, int sout_port, int reset_port);
 /* USB-IO2.0を用いた通信を終了する。
  * 成功と判定したら真、失敗を検出したら偽を返す。
  */
-int usbio_stop(struct avrio_t *avrio);
+int usbio_stop(avrio_t *avrio);
 /* USB-IO2.0を用いて8ビット送受信する。
  * 成功したら受信したデータを、失敗したら-1を返す。
  */
