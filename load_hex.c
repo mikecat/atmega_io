87842751c3fb5fd9e09c3d8b73299da38641a143
MikeCAT <mikecat@users.noreply.github.com>
2015-01-15 22:10:59 +0900

    implement chars_to_words

--- load_hex.c
+++ load_hex.c
@@ -5,5 +5,13 @@ int load_hex(char *out, int out_size, FILE *fp) {
 }
 
 int chars_to_words(unsigned int *out, const char *in, int in_bytes) {
+	int i;
+	if (out == NULL || in == NULL || in_bytes < 0 || in_bytes % 2 != 0) {
+		return LOAD_HEX_INVALID_PARAMETER;
+	}
+	for (i = 0; i < in_bytes; i += 2) {
+		out[i / 2] = ((unsigned char)in[i] & 0xff) |
+			(((unsigned int)in[i + 1] & 0xff) << 8);
+	}
 	return LOAD_HEX_SUCCESS;
 }
